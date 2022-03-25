dcd31fb62f73f59089ce9f7734315b9b6320fe04
Maribis27 <maribis8767@gmail.com>
2022-03-24 21:45:38 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivas';
+
+  estaLogueado: boolean = true;
+  nuevoUsuario: boolean = false;
+
+  lenguajeProgramacion: string [] = ['Javascript','Python','Java','C/C++','PHP', 'C#']; 
+
+
+  flagStiloClass: boolean = true;
+
+  cambiarFlag(){
+    this.flagStiloClass = !this.flagStiloClass;
+  }
+
 }
